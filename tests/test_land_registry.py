c34b2f4c0e7f9a51bc5aae0b60b8ea2759a204b7
Leonardo Urbano <urbanstyle_87@hotmail.it>
2023-08-20 22:37:04 +0100

    Rewritten tests

--- tests/test_land_registry.py
+++ tests/test_land_registry.py
@@ -3,11 +3,13 @@ from unittest.mock import Mock, patch
 from uk_land_property_client.land_registry import UKLandClient
 from selenium.webdriver.chrome.options import Options
 
+
 class TestUKLandClient(unittest.TestCase):
     """Test the UKLandClient class."""
 
     @patch('uk_land_property_client.land_registry.Datashelf')
-    def test_download_data_valid_postcode(self, mock_datashelf: Mock) -> None:
+    @patch('selenium.webdriver.chrome.webdriver.WebDriver')
+    def test_download_data_valid_postcode(self, mock_webdriver, mock_datashelf: Mock) -> None:
         """
         Test the `download_data` method for a valid postcode.
         This test case simulates the behavior of the `download_data` method in the `UKLandClient` class for a valid
@@ -34,11 +36,14 @@ class TestUKLandClient(unittest.TestCase):
             chrome_options.add_argument('--headless')
             chrome_options.binary_location = '/path/to/chrome/binary'  # Update with actual path
 
-            # Pass Chrome options to the WebDriver
-            client = UKLandClient("SW1A 1AA", webdriver_options=chrome_options)
-            downloaded_data = client.downloaded_data
+            # Use the context manager to temporarily patch the WebDriver behavior
+            with mock_webdriver as mock_webdriver_instance:
+                mock_webdriver_instance.return_value = mock_driver_instance
+                client = UKLandClient("SW1A 1AA")
+                downloaded_data = client.downloaded_data
 
-            self.assertIsNotNone(downloaded_data)  # You can add more assertions here
+                self.assertIsNotNone(downloaded_data)  # You can add more assertions here
+
 
 if __name__ == '__main__':
     unittest.main()
